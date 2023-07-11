91f0b54fc28f82352d19baf05754e9ff428e50ab
iamjooon2 <iamjooon2@gmail.com>
2023-07-11 19:58:30 +0900

    chore: 백엔드 프로젝트 생성

---
+++ backend/src/main/java/zipgo/ZipgoApplication.java
@@ -0,0 +1,13 @@
+package zipgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipgoApplication.class, args);
+	}
+
+}
---
+++ out/production/2023-zipgo/src/test/java/zipgo/ZipgoApplicationTests.java
@@ -0,0 +1,13 @@
+package zipgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
