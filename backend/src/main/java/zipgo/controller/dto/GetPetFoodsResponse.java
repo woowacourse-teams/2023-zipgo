c018ef4913f2d66a46e9aa76ad321a25b3c0dd2d
Rosie <dev.kayoung@gmail.com>
2023-07-13 11:52:23 +0900

    chore: 레코드로 변환

--- backend/src/main/java/zipgo/controller/dto/GetPetFoodsResponse.java
+++ backend/src/main/java/zipgo/controller/dto/GetPetFoodsResponse.java
@@ -2,14 +2,5 @@ package zipgo.controller.dto;
 
 import java.util.List;
 
-public class GetPetFoodsResponse {
-    private final List<PetFoodResponse> petFoods;
-
-    public GetPetFoodsResponse(List<PetFoodResponse> petFoods) {
-        this.petFoods = petFoods;
-    }
-
-    public List<PetFoodResponse> getPetFoods() {
-        return petFoods;
-    }
+public record GetPetFoodsResponse(List<PetFoodResponse> petFoods) {
 }
