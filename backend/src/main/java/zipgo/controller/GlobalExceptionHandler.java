ff6fbfa962aaee862517951290366e72c5b833a1
Rosie <dev.kayoung@gmail.com>
2023-07-16 20:32:20 +0900

    chore: ResponseStatus 어노테이션 제거

--- backend/src/main/java/zipgo/controller/GlobalExceptionHandler.java
+++ backend/src/main/java/zipgo/controller/GlobalExceptionHandler.java
@@ -3,7 +3,6 @@ package zipgo.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import zipgo.exception.KeywordException;
@@ -11,7 +10,6 @@ import zipgo.exception.KeywordException;
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler({KeywordException.NotFound.class})
-    @ResponseStatus(code = HttpStatus.NOT_FOUND)
     public ResponseEntity<String> handleNotFoundException(Exception exception) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
     }
