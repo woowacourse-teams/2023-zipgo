5302f05a790c647e5046386fa2ec7f94d422c8da
최원용 <rltgjqmduftlagl@gmail.com>
2023-07-17 16:32:59 +0900

    서버 배포 스크립트 작성 (#26)
    * feat: 서버 배포 스크립트 작성
    
    * chore: 사용중인 포트를 정상적으로 종료시킨다.

---
+++ backend/deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+PROJECT_NAME=zipgo-backend
+
+echo '> 현재 구동중인 애플리케이션 PID 확인'
+CURRENT_PID=$(sudo netstat -lntp | grep 8080 | awk '{print $7}' | cut -d'/' -f1)
+
+echo "현재 구동중인 애플리케이션 PID: $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
+    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+    echo "> kill -15 $CURRENT_PID"
+    sudo kill -15 $CURRENT_PID
+    sleep 5
+fi
+
+sudo chmod +x zipgo-backend-0.0.1-SNAPSHOT.jar
+sudo nohup java -jar \
+-Dspring.profiles.active=prod \
+-Dspring.config.import=env.properties \
+zipgo-backend-0.0.1-SNAPSHOT.jar > ~/application.log 2>&1 &
