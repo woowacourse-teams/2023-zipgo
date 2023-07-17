3da2909d4d226178848e52660407148080c2eacb
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2023-07-17 16:27:42 +0900

    chore: 사용중인 포트를 정상적으로 종료시킨다.

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -9,8 +9,8 @@ echo "현재 구동중인 애플리케이션 PID: $CURRENT_PID"
 if [ -z "$CURRENT_PID" ]; then
     echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-    echo "> kill -9 $CURRENT_PID"
-    sudo kill -9 $CURRENT_PID
+    echo "> kill -15 $CURRENT_PID"
+    sudo kill -15 $CURRENT_PID
     sleep 5
 fi
 
