2a769e98d10074dd65a7f6790770b1cac76fd2b1
Rosie <dev.kayoung@gmail.com>
2023-07-17 13:16:20 +0900

    refactor: string format 사용

--- backend/src/main/java/zipgo/exception/KeywordException.java
+++ backend/src/main/java/zipgo/exception/KeywordException.java
@@ -9,7 +9,7 @@ public class KeywordException extends RuntimeException {
     public static class NotFound extends KeywordException {
 
         public NotFound(String keywordName) {
-            super("이름이 " + keywordName + "인 키워드를 찾을 수 없습니다.");
+            super(String.format("이름이 $s 인 키워드를 찾을 수 없습니다.", keywordName));
         }
 
     }
