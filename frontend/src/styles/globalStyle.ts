6e498ce2da699c112665aaf1b5738e2d09ce066a
SangwonKang <72087183+ksone02@users.noreply.github.com>
2023-07-20 13:36:40 +0900

    feat/#12: Font 설정, p 태그 margin 제거

--- frontend/src/styles/globalStyle.ts
+++ frontend/src/styles/globalStyle.ts
@@ -2,12 +2,22 @@ import { css } from 'styled-components';
 import { normalize } from 'styled-normalize';
 
 const globalStyle = css`
+  @font-face {
+    font-family: Pretendard;
+    src: url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css')
+      format('css');
+  }
+
   ${normalize}
 
   * {
     box-sizing: border-box;
 
     font-size: 62.5%;
+  }
+
+  p {
+    margin: 0;
   }
 `;
 
