f2149fe71416c4b5f0bf9e686e42e214fdf8b538
iamjooon2 <iamjooon2@gmail.com>
2023-07-24 14:16:14 +0900

    feat: 중복파일 삭제

--- backend/src/main/java/zipgo/config/WebConfig.java
+++ backend/src/main/java/zipgo/config/WebConfig.java
@@ -6,19 +6,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import static org.springframework.http.HttpHeaders.LOCATION;
 
-
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    private static final String ALLOWED_METHOD_NAMES = "GET,POST,PUT,DELETE,PATCH";
-    private static final String DELIMITER = ",";
+    private static final String ALLOWED_METHODS = "*";
     private static final String PROD_ORIGIN = "*";
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+                .allowedMethods(ALLOWED_METHODS)
                 .allowedOrigins(PROD_ORIGIN)
-                .allowedMethods(ALLOWED_METHOD_NAMES.split(DELIMITER))
                 .exposedHeaders(LOCATION);
     }
 
