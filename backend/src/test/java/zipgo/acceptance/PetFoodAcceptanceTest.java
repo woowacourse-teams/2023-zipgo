f427825c382a9d15ddb8039a48cfa217834a1327
kyY00n <dev.kayoung@gmail.com>
2023-07-24 14:01:10 +0900

    feat: Swagger UI 설정 (#46)
    * feat: restdocs + swagger 설정
    
    * fix: 의존성 수정 및 명세 추가
    
    * fix: docs 삭제 task 실행 순서
    
    * feat: 생성한 문서를 정적리소스 위치로 복사
    
    * feat: UI 생성 테스크를 bootJar 태스크에 포함
    
    * fix: 빌드 스크립트 순서 조정
    
    * feat: 문서에 예외 응답 추가
    
    * chore: 문서를 버전관리에서 제외
    
    * chore: given when then 주석
    
    * chore: var 통일
    
    ---------
    
    Co-authored-by: wonyongChoi05 <rltgjqmduftlagl@gmail.com>

--- backend/src/test/java/zipgo/acceptance/PetFoodAcceptanceTest.java
+++ backend/src/test/java/zipgo/acceptance/PetFoodAcceptanceTest.java
@@ -1,68 +1,113 @@
 package zipgo.acceptance;
 
+import static com.epages.restdocs.apispec.RestAssuredRestDocumentationWrapper.resourceDetails;
 import static io.restassured.RestAssured.given;
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.queryParameters;
 
+import com.epages.restdocs.apispec.RestAssuredRestDocumentationWrapper;
+import com.epages.restdocs.apispec.Schema;
 import io.restassured.http.ContentType;
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
-import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import zipgo.controller.dto.ErrorResponse;
-import zipgo.controller.dto.GetPetFoodsResponse;
-import zipgo.controller.dto.PetFoodResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.restdocs.restassured.RestDocumentationFilter;
 
 public class PetFoodAcceptanceTest extends AcceptanceTest {
 
-    @Test
-    void 모든_식품_조회_API() {
-        ExtractableResponse<Response> response = given().contentType(ContentType.JSON)
-                .when().get("/pet-foods")
-                .then().extract();
+    @Nested
+    @DisplayName("식품 목록 조회 API")
+    class GetPetFoods {
 
-        GetPetFoodsResponse data = response.body().as(GetPetFoodsResponse.class);
+        @Test
+        void 키워드를_지정하지_않고_요청한다() {
+            // given
+            var 요청_준비 = given(spec)
+                    .contentType(ContentType.JSON)
+                    .filter(식품_목록_조회_API_문서_생성());
 
-        assertThat(response.statusCode()).isEqualTo(200);
-        assertThat(data.petFoods()).isNotEmpty();
-    }
+            // when
+            var 응답 = 요청_준비.when()
+                    .get("/pet-foods");
 
-    @Test
-    void petFoods를_foodList로_직렬화한다() {
-        //given
-        ExtractableResponse<Response> response = given().contentType(ContentType.JSON)
-                .when().get("/pet-foods")
-                .then().extract();
+            // then
+            응답.then()
+                    .assertThat().statusCode(HttpStatus.OK.value())
+                    .assertThat().body("foodList.size()", is(2));
+        }
 
-        //when
-        //then
-        List<PetFoodResponse> foodList = response.jsonPath().getList("foodList", PetFoodResponse.class);
-        assertThat(foodList).isNotEmpty();
-    }
+        private RestDocumentationFilter 식품_목록_조회_API_문서_생성() {
+            var 응답_형식 = Schema.schema("GetPetFoodResponse");
+            var 문서_정보 = resourceDetails().summary("식품 목록 조회 성공")
+                    .description("모든 반려동물 식품을 조회합니다.")
+                    .responseSchema(응답_형식);
 
-    @Test
-    void 키워드가_다이어트인_식품_조회_API() {
-        ExtractableResponse<Response> response = given().contentType(ContentType.JSON)
-                .queryParam("keyword", "diet")
-                .when().get("/pet-foods")
-                .then().extract();
+            return RestAssuredRestDocumentationWrapper.document("식품 목록 조회 성공",
+                    문서_정보,
+                    queryParameters(parameterWithName("keyword").optional().description("식품 조회 API")),
+                    responseFields(
+                            fieldWithPath("foodList").description("반려동물 식품 리스트"),
+                            fieldWithPath("foodList[].id").description("식품 id"),
+                            fieldWithPath("foodList[].name").description("식품 이름"),
+                            fieldWithPath("foodList[].imageUrl").description("식품 이미지 url"),
+                            fieldWithPath("foodList[].purchaseUrl").description("구매 링크")
+                    ));
+        }
 
-        GetPetFoodsResponse data = response.body().as(GetPetFoodsResponse.class);
+        @Test
+        void 키워드가_있는_목록을_요청한다() {
 
-        assertThat(response.statusCode()).isEqualTo(200);
-        assertThat(data.petFoods()).isNotEmpty();
-    }
+            // given
+            String 키워드 = "diet";
+            var 요청_준비 = given()
+                    .contentType(ContentType.JSON)
+                    .queryParam("keyword", 키워드);
 
-    @Test
-    void 존재하지_않는_키워드로_식품_조회_API() {
-        ExtractableResponse<Response> response = given().contentType(ContentType.JSON)
-                .queryParam("keyword", "존재하지 않는 키워드")
-                .when().get("/pet-foods")
-                .then().extract();
+            // when
+            var 응답 = 요청_준비.when()
+                    .get("/pet-foods");
 
-        ErrorResponse data = response.body().as(ErrorResponse.class);
+            // then
+            응답.then()
+                    .assertThat().statusCode(HttpStatus.OK.value())
+                    .assertThat().body("foodList.size()", not(empty()));
+        }
 
-        assertThat(response.statusCode()).isEqualTo(404);
-        assertThat(data.message()).isEqualTo("이름이 존재하지 않는 키워드 인 키워드를 찾을 수 없습니다.");
+        @Test
+        void 존재하지_않는_키워드로_요청한다() {
+            // given
+            var 요청_준비 = given(spec)
+                    .contentType(ContentType.JSON)
+                    .filter(존재하지_않는_키워드_예외_문서_생성())
+                    .queryParam("keyword", "존재하지 않는 키워드");
+
+            // when
+            var 응답 = 요청_준비.when()
+                    .get("/pet-foods");
+
+            // then
+            응답.then()
+                    .assertThat().statusCode(HttpStatus.NOT_FOUND.value())
+                    .assertThat().body("message", containsString("키워드를 찾을 수 없습니다."));
+
+        }
+
+        private RestDocumentationFilter 존재하지_않는_키워드_예외_문서_생성() {
+            var 응답_형식 = Schema.schema("ErrorResponse");
+            var 문서_정보 = resourceDetails().summary("식품 목록 조회 성공")
+                    .description("모든 반려동물 식품을 조회합니다.")
+                    .responseSchema(응답_형식);
+
+            return RestAssuredRestDocumentationWrapper.document("식품 목록 조회 실패", 문서_정보);
+        }
+
     }
 
 }
