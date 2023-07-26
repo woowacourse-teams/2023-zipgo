9efd0b74f0c234e92bf348a393d839d420a24ea7
Rosie <dev.kayoung@gmail.com>
2023-07-26 17:29:32 +0900

    feat: 빌더 추가

--- backend/src/test/java/zipgo/petfood/domain/fixture/PetFoodFixture.java
+++ backend/src/test/java/zipgo/petfood/domain/fixture/PetFoodFixture.java
@@ -6,16 +6,20 @@ import zipgo.petfood.domain.PetFood;
 public class PetFoodFixture {
 
     public static PetFood 키워드_없이_식품_초기화() {
-        return new PetFood("[고집] 돌아온 배배",
-                "https://github.com/woowacourse-teams/2023-zipgo",
-                "https://avatars.githubusercontent.com/u/94087228?v=4");
+        return PetFood.builder()
+                .name("[고집] 돌아온 배배")
+                .imageUrl("https://github.com/woowacourse-teams/2023-zipgo")
+                .purchaseLink("https://avatars.githubusercontent.com/u/94087228?v=4")
+                .build();
     }
 
-    public static PetFood 키워드_있이_식품_초기화(Keyword keyword) {
-        return new PetFood("[고집] 돌아온 배배",
-                "https://github.com/woowacourse-teams/2023-zipgo",
-                "https://avatars.githubusercontent.com/u/94087228?v=4",
-                keyword);
+    public static PetFood 키워드_있는_식품_초기화(Keyword keyword) {
+        return PetFood.builder()
+                .name("[고집] 돌아온 배배")
+                .imageUrl("https://github.com/woowacourse-teams/2023-zipgo")
+                .purchaseLink("https://avatars.githubusercontent.com/u/94087228?v=4")
+                .keyword(keyword)
+                .build();
     }
 
 }
