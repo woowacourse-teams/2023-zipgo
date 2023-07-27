48d3ba5371c77fb4694a01e35afb35a57230ab4b
chex <chex1004@gmail.com>
2023-07-27 22:19:19 +0900

    feat: 리뷰 추가 버튼 추가

--- frontend/src/components/Review/Reviews.tsx/ReviewList.tsx
+++ frontend/src/components/Review/Reviews.tsx/ReviewList.tsx
@@ -18,6 +18,9 @@ const ReviewList = (reviewListProps: ReviewListProps) => {
           <ReviewItem {...review} />
         </ReviewItemWrapper>
       ))}
+      <ReviewAddButton type="button" aria-label="리뷰 추가">
+        +
+      </ReviewAddButton>
     </ReviewListContainer>
   );
 };
@@ -36,4 +39,30 @@ const ReviewItemWrapper = styled.li`
   list-style: none;
 
   border-bottom: 1px solid ${({ theme }) => theme.color.grey200};
+`;
+
+const ReviewAddButton = styled.button`
+  all: unset;
+
+  cursor: pointer;
+
+  position: absolute;
+  right: 1.6rem;
+  bottom: 3.2rem;
+
+  display: flex;
+  justify-content: center;
+
+  width: 56px;
+  height: 56px;
+
+  font-size: 4rem;
+  font-weight: bold;
+  line-height: 60px;
+  color: ${({ theme }) => theme.color.white};
+  text-align: center;
+
+  background-color: ${({ theme }) => theme.color.primary};
+  border-radius: 50%;
+  box-shadow: 0 4px 4px rgb(0 0 0 / 25%);
 `;
