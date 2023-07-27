a0162f26a7f004ecd469bc201f54c7c20971f686
parkmuhyeun <pjhg410@gmail.com>
2023-07-27 13:51:53 +0900

    refactor: @Getter 추가

--- backend/src/main/java/zipgo/common/entity/BaseTimeEntity.java
+++ backend/src/main/java/zipgo/common/entity/BaseTimeEntity.java
@@ -2,12 +2,14 @@ package zipgo.common.entity;
 
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
+@Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseTimeEntity {
@@ -17,13 +19,5 @@ public abstract class BaseTimeEntity {
 
     @LastModifiedDate
     private LocalDateTime modifiedAt;
-
-    public LocalDateTime createdAt() {
-        return createdAt;
-    }
-
-    public LocalDateTime modifiedAt() {
-        return modifiedAt;
-    }
 
 }
