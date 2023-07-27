7e64058db00d707e466d92f0f9948a0b297c47d6
iamjooon2 <iamjooon2@gmail.com>
2023-07-27 14:29:49 +0900

    feat: MemberRepository 정의

---
+++ backend/src/main/java/zipgo/member/domain/repository/MemberRepository.java
@@ -0,0 +1,12 @@
+package zipgo.member.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import zipgo.member.domain.Member;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByEmail(String email);
+
+}
