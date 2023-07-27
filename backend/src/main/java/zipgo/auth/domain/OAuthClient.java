441487413781b8ae442d5cb40ed94ec068f39dc5
iamjooon2 <iamjooon2@gmail.com>
2023-07-27 15:26:16 +0900

    feat: OAuthClient 인터페이스 정의

---
+++ backend/src/main/java/zipgo/auth/domain/OAuthClient.java
@@ -0,0 +1,10 @@
+package zipgo.auth.domain;
+
+import zipgo.auth.OAuthResponse;
+
+public interface OAuthClient {
+
+    public String getAccessToken(String authCode);
+
+    public OAuthResponse getMemberDetail(String accessToken);
+}
