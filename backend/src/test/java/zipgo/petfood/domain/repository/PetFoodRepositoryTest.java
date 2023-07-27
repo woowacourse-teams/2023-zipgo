8d8ebb78bb97a942d114e7b439efb674a383da0f
Rosie <dev.kayoung@gmail.com>
2023-07-27 13:23:09 +0900

    test: 리포지토리 아이디로 조회 테스트

--- backend/src/test/java/zipgo/petfood/domain/repository/PetFoodRepositoryTest.java
+++ backend/src/test/java/zipgo/petfood/domain/repository/PetFoodRepositoryTest.java
@@ -1,13 +1,17 @@
 package zipgo.petfood.domain.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static zipgo.petfood.domain.fixture.PetFoodFixture.키워드_없이_식품_초기화;
 import static zipgo.petfood.domain.fixture.PetFoodFixture.키워드_있는_식품_초기화;
 
 import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import zipgo.petfood.domain.Keyword;
@@ -53,6 +57,34 @@ class PetFoodRepositoryTest {
     private PetFood 키워드있는_식품_생성하기() {
         Keyword 키워드 = keywordRepository.findAll().get(0);
         return petFoodRepository.save(키워드_있는_식품_초기화(키워드));
+    }
+
+    @Test
+    void 아이디로_식품을_조회할_수_있다() {
+        //given
+        PetFood 테스트_식품 = 키워드_없이_식품_초기화();
+        Long 아이디 = petFoodRepository.save(테스트_식품).getId();
+
+        //when
+        Optional<PetFood> optional_식품 = petFoodRepository.findById(아이디);
+
+        //then
+        assertAll(() -> {
+            assertThat(optional_식품).isNotEmpty();
+
+            PetFood 조회한_식품 = optional_식품.orElseThrow();
+            assertThat(조회한_식품).isEqualTo(테스트_식품);
+        });
+    }
+
+    @ParameterizedTest
+    @ValueSource(longs = {-1L, 999999999999999L, 0L})
+    void 존재하지_않는_아이디로_조회하면_빈_옵셔널을_반환한다(Long 존재하지_않는_아이디) {
+        //when
+        Optional<PetFood> optional_식품 = petFoodRepository.findById(존재하지_않는_아이디);
+
+        //then
+        assertThat(optional_식품).isEmpty();
     }
 
 }
