b66fa55c3d253d7a43683c1941b4deb53e51ad64
parkmuhyeun <pjhg410@gmail.com>
2023-07-27 13:24:03 +0900

    feat: BaseTimeEntity 생성

---
+++ backend/src/main/java/zipgo/common/entity/BaseTimeEntity.java
@@ -0,0 +1,29 @@
+package zipgo.common.entity;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
+
+    public LocalDateTime createdAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime modifiedAt() {
+        return modifiedAt;
+    }
+
+}
