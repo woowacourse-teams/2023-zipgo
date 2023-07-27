7226c15229c55aebd0c421596e29bef0c8ab5a20
최원용 <rltgjqmduftlagl@gmail.com>
2023-07-27 15:24:57 +0900

    support: Querydsl 의존성 및 환경 설정 (#79)
    * support: Querydsl 의존성 및 환경 설정
    
    * feat: Querydsl Config
    
    * feat: jacoco BaseTimeEntity

--- backend/src/main/java/zipgo/petfood/domain/PetFood.java
+++ backend/src/main/java/zipgo/petfood/domain/PetFood.java
@@ -8,7 +8,6 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
