475171a7fa3ba3f24e0e3541657b0f20fa603c77
iamjooon2 <iamjooon2@gmail.com>
2023-07-28 12:26:51 +0900

    refactor: memberService에 CQRS 패턴 적용

--- backend/src/main/java/zipgo/auth/application/AuthService.java
+++ backend/src/main/java/zipgo/auth/application/AuthService.java
@@ -6,6 +6,8 @@ import org.springframework.transaction.annotation.Transactional;
 import zipgo.auth.OAuthResponse;
 import zipgo.auth.util.JwtTokenProvider;
 import zipgo.member.domain.Member;
+import zipgo.member.domain.application.MemberCommandService;
+import zipgo.member.domain.application.MemberQueryService;
 import zipgo.member.domain.repository.MemberRepository;
 
 import java.util.Optional;
@@ -17,19 +19,20 @@ public class AuthService {
 
     private final OAuthClient oAuthClient;
     private final JwtTokenProvider jwtTokenProvider;
-    private final MemberRepository memberRepository;
+    private final MemberQueryService memberQueryService;
+    private final MemberCommandService memberCommandService;
 
     public String createToken(String authCode) {
         String accessToken = oAuthClient.getAccessToken(authCode);
         OAuthResponse memberDetail = oAuthClient.getMemberDetail(accessToken);
 
-        Optional<Member> memberOptional = memberRepository.findByEmail(memberDetail.getEmail());
+        Optional<Member> memberOptional = memberCommandService.findByEmail(memberDetail.getEmail());
         if (memberOptional.isPresent()) {
             return jwtTokenProvider.create(String.valueOf(memberOptional.get().getId()));
         }
 
         Member member = new Member(memberDetail.getNickName(), memberDetail.getEmail());
-        Long memberId = memberRepository.save(member).getId();
+        Long memberId = memberQueryService.save(member).getId();
         return jwtTokenProvider.create(String.valueOf(memberId));
     }
 
---
+++ backend/src/main/java/zipgo/member/domain/application/MemberQueryService.java
@@ -0,0 +1,19 @@
+package zipgo.member.domain.application;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import zipgo.member.domain.Member;
+import zipgo.member.domain.repository.MemberRepository;
+
+@Transactional(readOnly = true)
+@Service
+@RequiredArgsConstructor
+public class MemberQueryService {
+
+    private final MemberRepository memberRepository;
+
+    public Member save(Member member) {
+        return memberRepository.save(member);
+    }
+}
