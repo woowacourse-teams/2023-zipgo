969ef6f7b6bfba17448c3b5177cb24deb5c47dfa
n0eyes <doll4586633@naver.com>
2023-07-31 14:41:17 +0900

    refactor: Checkbox 로직 수정
    - 공통 로직 추상화
    - 기본 input 속성 적용

---
+++ frontend/src/utils/compound.ts
@@ -0,0 +1,32 @@
+import { Children, isValidElement, PropsWithChildren, ReactElement, ReactNode } from 'react';
+
+type WithAsChild<P> = P & {
+  asChild: true;
+  child: ReactElement<P>;
+};
+
+type WithoutAsChild<P> = P & {
+  asChild: false;
+  child: null;
+};
+
+// eslint-disable-next-line comma-spacing
+const getValidChildAsChild = <P>(children: ReactNode) => {
+  const child = Children.only(children);
+
+  return isValidElement<P>(child) ? child : null;
+};
+
+export function getValidProps<P extends PropsWithChildren<{ asChild?: boolean }>>(
+  props: P,
+): WithAsChild<P> | WithoutAsChild<P> {
+  const { asChild, children, ...restProps } = props;
+
+  if (asChild) {
+    const child = getValidChildAsChild<P>(children);
+
+    if (child) return { ...restProps, asChild, child };
+  }
+
+  return { ...restProps, asChild: false, child: null };
+}
