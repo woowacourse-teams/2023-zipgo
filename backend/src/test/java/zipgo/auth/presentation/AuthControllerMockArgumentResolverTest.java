3accd0cf66862d4dc296c76d63bc6c988be1b90b
iamjooon2 <iamjooon2@gmail.com>
2023-07-31 22:36:45 +0900

    test: AuthController 테스트 작성

--- backend/src/test/java/zipgo/auth/presentation/AuthControllerMockArgumentResolverTest.java
+++ backend/src/test/java/zipgo/auth/presentation/AuthControllerMockArgumentResolverTest.java
@@ -1,5 +1,7 @@
 package zipgo.auth.presentation;
 
+import com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper;
+import com.epages.restdocs.apispec.Schema;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
@@ -8,6 +10,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
@@ -15,20 +21,30 @@ import org.springframework.web.servlet.mvc.method.annotation.ExceptionHandlerExc
 import zipgo.auth.presentation.dto.AuthDto;
 import zipgo.member.application.MemberQueryService;
 
+import static com.epages.restdocs.apispec.RestAssuredRestDocumentationWrapper.resourceDetails;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.queryParameters;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static zipgo.member.domain.fixture.MemberFixture.식별자_있는_멤버;
 
-@ExtendWith(MockitoExtension.class)
+
 @SuppressWarnings("NonAsciiCharacters")
+@ExtendWith({MockitoExtension.class, RestDocumentationExtension.class})
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class AuthControllerMockArgumentResolverTest {
 
     private MockMvc mockMvc;
+    private HandlerMethodArgumentResolver mockArgumentResolver = mock(JwtArgumentResolver.class);
 
     @InjectMocks
     private AuthController authController;
@@ -36,12 +52,11 @@ class AuthControllerMockArgumentResolverTest {
     @Mock
     private MemberQueryService memberQueryService;
 
-    private HandlerMethodArgumentResolver mockArgumentResolver = mock(JwtArgumentResolver.class);
-
     @BeforeEach
-    void setUp() {
+    void setUp(RestDocumentationContextProvider restDocumentationContextProvider) {
         mockMvc = MockMvcBuilders.standaloneSetup(authController)
                 .setControllerAdvice(new ExceptionHandlerExceptionResolver())
+                .apply(documentationConfiguration(restDocumentationContextProvider))
                 .setCustomArgumentResolvers(mockArgumentResolver)
                 .build();
     }
@@ -58,10 +73,30 @@ class AuthControllerMockArgumentResolverTest {
 
         // when
         var 요청 = mockMvc.perform(get("/auth")
-                .header(AUTHORIZATION, "Bearer 1a.2a.3b"));
+                        .header(AUTHORIZATION, "Bearer 1a.2a.3b"))
+                .andDo(문서_생성());
 
         // then
         var 응답 = 요청.andExpect(status().isOk());
     }
 
+
+    private RestDocumentationResultHandler 문서_생성() {
+        var 응답_형식 = Schema.schema("AuthResponse");
+        var 문서_정보 = resourceDetails().summary("사용자 정보 확인")
+                .description("토큰을 서버로 보내 사용자 정보를 받습니다.")
+                .responseSchema(응답_형식);
+
+        return MockMvcRestDocumentationWrapper.document("사용자 정보 확인",
+                문서_정보,
+                requestHeaders(
+                        headerWithName(AUTHORIZATION).description("Bearer + accessToken")
+                ),
+                responseFields(
+                        fieldWithPath("name").description("사용자 이름"),
+                        fieldWithPath("profileImgUrl").description("사용자 프로필 사진")
+                ));
+    }
+
 }
+
--- backend/src/test/java/zipgo/auth/presentation/AuthControllerTest.java
+++ backend/src/test/java/zipgo/auth/presentation/AuthControllerTest.java
@@ -1,6 +1,6 @@
 package zipgo.auth.presentation;
 
-import com.epages.restdocs.apispec.RestAssuredRestDocumentationWrapper;
+import com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper;
 import com.epages.restdocs.apispec.Schema;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
@@ -11,7 +11,7 @@ import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDoc
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
-import org.springframework.restdocs.restassured.RestDocumentationFilter;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import zipgo.auth.application.AuthService;
@@ -20,11 +20,11 @@ import zipgo.member.application.MemberQueryService;
 
 import static com.epages.restdocs.apispec.RestAssuredRestDocumentationWrapper.resourceDetails;
 import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.queryParameters;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static zipgo.member.domain.fixture.MemberFixture.식별자_있는_멤버;
 
@@ -63,28 +63,27 @@ class AuthControllerTest {
 
         // when
         var 요청 = mockMvc.perform(post("/auth/login")
-                .param("code", "인가_코드"));
+                .param("code", "인가_코드"))
+                .andDo(소셜_로그인_문서_생성());
 
         // then
         var 응답 = 요청.andExpect(status().isOk());
     }
 
-    private RestDocumentationFilter 소셜_로그인_문서_생성() {
+    private RestDocumentationResultHandler 소셜_로그인_문서_생성() {
         var 응답_형식 = Schema.schema("TokenResponse");
         var 문서_정보 = resourceDetails().summary("로그인 성공")
                 .description("소셜 로그인을 시도합니다")
                 .responseSchema(응답_형식);
 
-        return RestAssuredRestDocumentationWrapper.document("소셜 로그인 성공",
+        return MockMvcRestDocumentationWrapper.document("소셜 로그인 성공",
                 문서_정보,
-                queryParameters(parameterWithName("keyword").optional().description("소셜 로그인 API")),
+                queryParameters(parameterWithName("code").optional().description("소셜 로그인 API")),
                 responseFields(
-                        fieldWithPath("tokenResponse").description("로그인 후 생성된 토큰 정보"),
-                        fieldWithPath("tokenResponse[].accessToken").description("accessToken"),
-                        fieldWithPath("tokenResponse[].authResponse[]").description("로그인 후 필요한 사용자 정보"),
-                        fieldWithPath("tokenResponse[].authResponse[].name").description("사용자 이름"),
-                        fieldWithPath("tokenResponse[].authResponse[].profileImgUrl").description("사용자 프로필 사진")
-
+                        fieldWithPath("accessToken").description("accessToken"),
+                        fieldWithPath("authResponse").description("로그인 후 필요한 사용자 정보"),
+                        fieldWithPath("authResponse.name").description("사용자 이름"),
+                        fieldWithPath("authResponse.profileImgUrl").description("사용자 프로필 사진")
                 ));
     }
 
