1fb82fbae3f1661e440f2b3919687a5d29028897
Rosie <dev.kayoung@gmail.com>
2023-08-01 11:17:03 +0900

    refactor: 요청 스키마에 타입 추가

--- backend/src/test/java/zipgo/review/presentation/ReviewControllerTest.java
+++ backend/src/test/java/zipgo/review/presentation/ReviewControllerTest.java
@@ -5,6 +5,7 @@ import com.epages.restdocs.apispec.Schema;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.restdocs.restassured.RestDocumentationFilter;
 import zipgo.acceptance.AcceptanceTest;
 
@@ -14,8 +15,13 @@ import static com.epages.restdocs.apispec.Schema.schema;
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.Matchers.is;
-import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
+import static org.springframework.http.HttpStatus.OK;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
@@ -24,6 +30,7 @@ import static zipgo.review.fixture.ReviewFixture.리뷰_수정_요청;
 
 public class ReviewControllerTest extends AcceptanceTest {
 
+    // TODO: API 문서에 헤더 정보 추가
     //TODO 갈비꺼 merge 후 고정 상수 리팩터링하기
 
     @Nested
@@ -59,14 +66,14 @@ public class ReviewControllerTest extends AcceptanceTest {
                     문서_정보.responseSchema(성공_응답_형식),
                     pathParameters(parameterWithName("id").description("식품 id")),
                     responseFields(
-                            fieldWithPath("[].id").description("리뷰 id"),
-                            fieldWithPath("[].reviewerName").description("리뷰 작성자 이름"),
-                            fieldWithPath("[].rating").description("리뷰 별점"),
-                            fieldWithPath("[].date").description("리뷰 생성일"),
-                            fieldWithPath("[].comment").description("리뷰 코멘트"),
-                            fieldWithPath("[].tastePreference").description("기호성"),
-                            fieldWithPath("[].stoolCondition").description("대변 상태"),
-                            fieldWithPath("[].adverseReactions").description("이상 반응들")
+                            fieldWithPath("[].id").description("리뷰 id").type(JsonFieldType.NUMBER),
+                            fieldWithPath("[].reviewerName").description("리뷰 작성자 이름").type(JsonFieldType.STRING),
+                            fieldWithPath("[].rating").description("리뷰 별점").type(JsonFieldType.NUMBER),
+                            fieldWithPath("[].date").description("리뷰 생성일").type(JsonFieldType.STRING),
+                            fieldWithPath("[].comment").description("리뷰 코멘트").type(JsonFieldType.STRING),
+                            fieldWithPath("[].tastePreference").description("기호성").type(JsonFieldType.STRING),
+                            fieldWithPath("[].stoolCondition").description("대변 상태").type(JsonFieldType.STRING),
+                            fieldWithPath("[].adverseReactions").description("이상 반응들").type(JsonFieldType.ARRAY)
                     ));
         }
 
@@ -76,10 +83,11 @@ public class ReviewControllerTest extends AcceptanceTest {
     @DisplayName("리뷰 생성 API")
     class CreateReviews {
 
-        private Schema 성공_응답_형식 = schema("CreateReviewResponse");
         private ResourceSnippetDetails 문서_정보 = resourceDetails()
                 .summary("리뷰 생성하기")
                 .description("해당 반려동물 식품에 대한 리뷰를 생성합니다.");
+        private Schema 요청_형식 = schema("CreateReviewRequest");
+        private Schema 성공_응답_형식 = schema("CreateReviewResponse");
 
         @Test
         void 리뷰를_성공적으로_생성하면_201_반환() {
@@ -102,8 +110,15 @@ public class ReviewControllerTest extends AcceptanceTest {
 
         private RestDocumentationFilter 리뷰_생성_API_문서_생성() {
             return document("리뷰 생성 - 성공",
-                    문서_정보.responseSchema(성공_응답_형식)
-            );
+                    문서_정보.requestSchema(요청_형식).responseSchema(성공_응답_형식),
+                    requestFields(
+                            fieldWithPath("petFoodId").description("식품 id").type(JsonFieldType.NUMBER),
+                            fieldWithPath("rating").description("리뷰 별점").type(JsonFieldType.NUMBER),
+                            fieldWithPath("comment").description("리뷰 코멘트").optional().type(JsonFieldType.STRING),
+                            fieldWithPath("tastePreference").description("기호성").type(JsonFieldType.STRING),
+                            fieldWithPath("stoolCondition").description("대변 상태").type(JsonFieldType.STRING),
+                            fieldWithPath("adverseReactions").description("이상 반응들").type(JsonFieldType.ARRAY)
+                    ));
         }
 
         @Test
@@ -134,6 +149,7 @@ public class ReviewControllerTest extends AcceptanceTest {
     @DisplayName("리뷰 수정 API")
     class UpdateReviews {
 
+        private Schema 요청_형식 = schema("UpdateReviewRequest");
         private Schema 성공_응답_형식 = schema("UpdateReviewResponse");
         private ResourceSnippetDetails 문서_정보 = resourceDetails()
                 .summary("리뷰 수정하기")
@@ -160,9 +176,15 @@ public class ReviewControllerTest extends AcceptanceTest {
 
         private RestDocumentationFilter 리뷰_수정_API_문서_생성() {
             return document("리뷰 수정 - 성공",
-                    문서_정보.responseSchema(성공_응답_형식),
-                    pathParameters(parameterWithName("reviewId").description("리뷰 id"))
-            );
+                    문서_정보.requestSchema(요청_형식).responseSchema(성공_응답_형식),
+                    pathParameters(parameterWithName("reviewId").description("리뷰 id")),
+                    requestFields(
+                            fieldWithPath("rating").description("리뷰 별점").type(JsonFieldType.NUMBER),
+                            fieldWithPath("comment").description("리뷰 코멘트").optional().type(JsonFieldType.STRING),
+                            fieldWithPath("tastePreference").description("기호성").type(JsonFieldType.STRING),
+                            fieldWithPath("stoolCondition").description("대변 상태").type(JsonFieldType.STRING),
+                            fieldWithPath("adverseReactions").description("이상 반응들").type(JsonFieldType.ARRAY)
+                    ));
         }
 
         @Test
