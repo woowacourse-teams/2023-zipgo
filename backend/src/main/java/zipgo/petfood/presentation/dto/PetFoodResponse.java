4ac1bdc6c48015eaf8c54909b817b3f4094357af
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2023-08-02 00:02:04 +0900

    refactor: 식품 목록 응답 조회 수정

--- backend/src/main/java/zipgo/petfood/presentation/dto/PetFoodResponse.java
+++ backend/src/main/java/zipgo/petfood/presentation/dto/PetFoodResponse.java
@@ -2,12 +2,19 @@ package zipgo.petfood.presentation.dto;
 
 import zipgo.petfood.domain.PetFood;
 
-public record PetFoodResponse(Long id, String imageUrl, String name, String purchaseUrl) {
+public record PetFoodResponse(
+        Long id,
+        String imageUrl,
+        String brandName,
+        String foodName,
+        String purchaseUrl
+) {
 
     public static PetFoodResponse from(PetFood petFood) {
         return new PetFoodResponse(
                 petFood.getId(),
                 petFood.getImageUrl(),
+                petFood.getBrand().getName(),
                 petFood.getName(),
                 petFood.getPurchaseLink()
         );
