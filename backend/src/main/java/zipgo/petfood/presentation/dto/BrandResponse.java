a3ac95d6f82d074cc81adfe249dc68547d60cf18
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2023-08-02 16:23:56 +0900

    chore: 브랜드 Image Url 추가

--- backend/src/main/java/zipgo/petfood/presentation/dto/BrandResponse.java
+++ backend/src/main/java/zipgo/petfood/presentation/dto/BrandResponse.java
@@ -9,8 +9,7 @@ public record BrandResponse(
 ) {
 
     public static BrandResponse from(Brand brand) {
-        // TODO: brand.getName 바꿔야함(2번째 프로퍼티)
-        return new BrandResponse(brand.getId(), brand.getName(), brand.getName());
+        return new BrandResponse(brand.getId(), brand.getImageUrl(), brand.getName());
     }
 
 }
