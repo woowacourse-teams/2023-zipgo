1c77453fe47a9cfa5192f97ec5542c0964825ce3
n0eyes <doll4586633@naver.com>
2023-08-02 17:54:07 +0900

    feat: FoodListFilterKeyword 한글, 영어 상수 추가

---
+++ frontend/src/constants/food.ts
@@ -0,0 +1,10 @@
+import { KeywordsEn, KeywordsKo } from '@/types/food/client';
+
+export const keywordsKo: KeywordsKo = ['영양기준', '주원료', '브랜드', '기능성'];
+
+export const keywordsEn: KeywordsEn = [
+  'nutritionStandard',
+  'mainIngredients',
+  'brands',
+  'functionality',
+];
