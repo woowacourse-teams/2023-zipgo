fbb9a392954b503c7e9d74cc48c2fa58b50972be
n0eyes <doll4586633@naver.com>
2023-08-03 20:51:32 +0900

    feat: 헤더 로그인 버튼 추가

--- frontend/src/components/@common/Header/Header.tsx
+++ frontend/src/components/@common/Header/Header.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { styled } from 'styled-components';
 
 import ZipgoLogo from '@/assets/svg/zipgo_logo_light.svg';
@@ -5,18 +6,34 @@ import ZipgoLogo from '@/assets/svg/zipgo_logo_light.svg';
 const Header = () => (
   <HeaderContainer>
     <img src={ZipgoLogo} alt="집사의고민 로고" />
+    <LinkButton to="login">로그인</LinkButton>
   </HeaderContainer>
 );
 
 export default Header;
 
 const HeaderContainer = styled.header`
-  position: relative;
+  position: absolute;
   z-index: 9999;
+  top: 0;
+  left: 0;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 
   width: 100vw;
   height: 8rem;
   padding: 2rem;
 
   background: transparent;
+`;
+
+const LinkButton = styled(Link)`
+  font-size: 1.6rem;
+  font-weight: 700;
+  line-height: 1.7rem;
+  color: ${({ theme }) => theme.color.white};
+  text-decoration: none;
+  letter-spacing: -0.5px;
 `;
