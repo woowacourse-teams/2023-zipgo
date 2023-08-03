a69032ed60a858765b5c9734200b0d87d346ecc8
최원용 <rltgjqmduftlagl@gmail.com>
2023-08-03 11:07:13 +0900

    refactor: 식품 필터링 조회 API 스펙 수정 (#127)
    * refactor: 조회 API 수정
    
    * chore: 로그 삭제
    
    * refactor: 중복 로직 제거
    
    * refactor: static 제거
    
    * refactor: query string 쉼표(,) 적용
    
    * refactor: query string UTF-8로 decode

--- backend/src/main/java/zipgo/petfood/application/PetFoodQueryService.java
+++ backend/src/main/java/zipgo/petfood/application/PetFoodQueryService.java
@@ -4,11 +4,13 @@ import static java.util.Arrays.asList;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import zipgo.brand.domain.Brand;
 import zipgo.brand.domain.repository.BrandRepository;
+import zipgo.petfood.domain.HasStandard;
 import zipgo.petfood.domain.PetFood;
 import zipgo.petfood.domain.repository.PetFoodQueryRepository;
 import zipgo.petfood.domain.repository.PetFoodRepository;
@@ -22,6 +24,59 @@ public class PetFoodQueryService {
     private final BrandRepository brandRepository;
     private final PetFoodRepository petFoodRepository;
     private final PetFoodQueryRepository petFoodQueryRepository;
+
+    public List<PetFood> getPetFoods(
+            List<String> brandsName,
+            List<String> nutrientStandards,
+            List<String> primaryIngredientList,
+            List<String> functionalityList
+    ) {
+        return petFoodRepository.findAll().stream()
+                .filter(petFood -> isValidStandard(nutrientStandards, petFood.getHasStandard())
+                        && isContainBrandsName(brandsName, petFood.getBrand().getName())
+                        && isContainMainIngredients(petFood.getPrimaryIngredients(), primaryIngredientList)
+                        && isContainFunctionalities(petFood.getFunctionality(), functionalityList))
+                .collect(Collectors.toList());
+    }
+
+    private boolean isContainBrandsName(List<String> brandsName, String brandName) {
+        if (brandsName.isEmpty()) {
+            return true;
+        }
+        return brandsName.contains(brandName);
+    }
+
+    private boolean isValidStandard(List<String> nutrientStandards, HasStandard hasStandard) {
+        if (nutrientStandards.isEmpty()) {
+            return true;
+        }
+        boolean isValid = true;
+        for (String nutrientStandard : nutrientStandards) {
+            if (nutrientStandard.equals("유럽") && !hasStandard.getEurope()) {
+                isValid = false;
+            }
+            if (nutrientStandard.equals("미국") && !hasStandard.getUnitedStates()) {
+                isValid = false;
+            }
+        }
+        return isValid;
+    }
+
+    private boolean isContainMainIngredients(List<String> primaryIngredients, List<String> primaryIngredientList) {
+        if (primaryIngredientList.isEmpty()) {
+            return true;
+        }
+        return primaryIngredientList.stream()
+                .anyMatch(it -> primaryIngredients.contains(it));
+    }
+
+    private boolean isContainFunctionalities(List<String> functionalities, List<String> functionalityList) {
+        if (functionalityList.isEmpty()) {
+            return true;
+        }
+        return functionalities.stream()
+                .anyMatch(it -> functionalities.contains(it));
+    }
 
     public List<PetFood> getPetFoodByDynamicValue(String keyword, String brand, String primaryIngredients) {
         return petFoodQueryRepository.findPetFoods(keyword, brand, primaryIngredients);
