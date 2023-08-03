61f2fd29a14b6050f14344701da56d7b9e8bc6d1
iamjooon2 <iamjooon2@gmail.com>
2023-08-03 17:22:22 +0900

    refactor: 멤버 도메인 옵션 추가

--- backend/src/main/java/zipgo/member/domain/Member.java
+++ backend/src/main/java/zipgo/member/domain/Member.java
@@ -3,6 +3,7 @@ package zipgo.member.domain;
 import static lombok.AccessLevel.PROTECTED;
 import static lombok.EqualsAndHashCode.Include;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -26,10 +27,13 @@ public class Member {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String name;
 
+    @Column(unique = true, nullable = false)
     private String email;
 
+    @Column(nullable = false)
     private String profileImgUrl;
 
 }
