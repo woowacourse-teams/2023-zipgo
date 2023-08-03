052dafd03899ea1a5b10edd66d7f957800e69cb7
n0eyes <doll4586633@naver.com>
2023-08-03 15:48:15 +0900

    feat: Template css position 속성 제거

--- frontend/src/components/@common/Template.tsx
+++ frontend/src/components/@common/Template.tsx
@@ -76,8 +76,6 @@ const Container = styled.div<
     }
   | { isFixedHeader?: never; headerHeight?: never }
 >`
-  position: absolute;
-
   width: 100%;
   height: 100%;
   min-height: calc((var(--vh, 1vh) * 100));
