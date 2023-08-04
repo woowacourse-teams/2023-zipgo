55ecb36376d493c2316171599f08f07b275c764c
n0eyes <doll4586633@naver.com>
2023-08-04 12:38:23 +0900

    feat: api 라이프 사이클에 인증 로직 추가

--- frontend/src/router/Router.tsx
+++ frontend/src/router/Router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import App from '@/App';
+import AxiosInterceptors from '@/components/@common/AxiosInterceptors/AxiosInterceptors';
 import FoodDetail from '@/pages/FoodDetail/FoodDetail';
 import Landing from '@/pages/Landing/Landing';
 import Login from '@/pages/Login/Login';
@@ -10,7 +11,11 @@ import ReviewStarRating from '@/pages/ReviewStarRating/ReviewStarRating';
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />,
+    element: (
+      <AxiosInterceptors>
+        <App />
+      </AxiosInterceptors>
+    ),
     errorElement: <div>error</div>,
     children: [
       {
