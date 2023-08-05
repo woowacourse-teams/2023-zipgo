fbef9592b7662763697923c46e7811ac28d10c92
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2023-08-05 19:01:37 +0900

    refactor: data.sql 실행 방지

--- backend/src/test/java/zipgo/common/service/QueryServiceTest.java
+++ backend/src/test/java/zipgo/common/service/QueryServiceTest.java
@@ -2,15 +2,15 @@ package zipgo.common.service;
 
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
 
-@SpringBootTest
 @Transactional(readOnly = true)
 @SuppressWarnings("NonAsciiCharacters")
-@Sql(scripts = {"classpath:truncate.sql"})
+@SpringBootTest(properties = {"spring.sql.init.mode=never"})
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class QueryServiceTest {
 
 }
