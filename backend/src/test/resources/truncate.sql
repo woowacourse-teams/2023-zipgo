f687aa678932a477e4bc0d98ee0140a0316e39ad
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2023-08-05 19:23:54 +0900

    refactor: PetFoodController 인수 테스트 리팩터링

--- backend/src/test/resources/truncate.sql
+++ backend/src/test/resources/truncate.sql
@@ -1,8 +1,9 @@
 SET FOREIGN_KEY_CHECKS = 0;
 truncate table adverse_reaction;
 truncate table review;
-truncate table `member`;
+truncate table member;
+truncate table primary_ingredient;
+truncate table functionality;
 truncate table pet_food;
-truncate table keyword;
 truncate table brand;
 SET FOREIGN_KEY_CHECKS = 1;
