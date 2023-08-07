c8a50b709a524276b879ad3bc72d6908cb13c76e
iamjooon2 <iamjooon2@gmail.com>
2023-08-07 15:25:28 +0900

    refactor: 엣지케이스 추가

--- backend/src/test/java/zipgo/auth/support/BearerTokenExtractorTest.java
+++ backend/src/test/java/zipgo/auth/support/BearerTokenExtractorTest.java
@@ -3,6 +3,8 @@ package zipgo.auth.support;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.mock.web.MockHttpServletRequest;
 import zipgo.auth.exception.AuthException;
 
@@ -37,11 +39,12 @@ class BearerTokenExtractorTest {
                 .hasMessageContaining("인증이 필요합니다");
     }
 
-    @Test
-    void 토큰_형식이_유효하지_않으면_예외가_발생한다() {
+    @ParameterizedTest(name = "토큰 형식이 유효하지 않으면 예외가 발생한다 name = {0}")
+    @ValueSource(strings = {"Bearer 내맘대로토큰", "nonBearer 니맘대로토큰"})
+    void 토큰_형식이_유효하지_않으면_예외가_발생한다(String token) {
         // given
         var 요청 = new MockHttpServletRequest();
-        요청.addHeader("Authorization", "Bearer 내맘대로토큰");
+        요청.addHeader("Authorization", token);
 
         // expect
         assertThatThrownBy(() -> BearerTokenExtractor.extract(요청))
