53bb96c718af1a9f91c53a4114e2ad8f52f6f5e4
iamjooon2 <iamjooon2@gmail.com>
2023-08-09 10:33:01 +0900

    refactor: 멤버 도메인 제약 조건 추가 (#141)
    * refactor: 멤버 도메인 제약조건 추가
    
    * refactor: profileImgUrl nullable 옵션 추가

--- backend/src/main/java/zipgo/member/domain/Member.java
+++ backend/src/main/java/zipgo/member/domain/Member.java
@@ -1,8 +1,6 @@
 package zipgo.member.domain;
 
-import static lombok.AccessLevel.PROTECTED;
-import static lombok.EqualsAndHashCode.Include;
-
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,6 +10,9 @@ import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import static lombok.AccessLevel.PROTECTED;
+import static lombok.EqualsAndHashCode.Include;
 
 @Entity
 @Getter
@@ -26,10 +27,13 @@ public class Member {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String name;
 
+    @Column(unique = true, nullable = false)
     private String email;
 
+    @Column(nullable = false)
     private String profileImgUrl;
 
 }
