02d738120b1fb4a0cc6dcfa2266daf103055e0fa
kyY00n <dev.kayoung@gmail.com>
2023-08-09 12:53:25 +0900

    fix: 리뷰 평균을 소수점 한자리 수까지만 응답하도록 함 (#201)
    * fix: 소수점 한자리수까지만 응답하도록 함
    
    * refactor: 메서드 분리

--- backend/src/main/java/zipgo/petfood/domain/Reviews.java
+++ backend/src/main/java/zipgo/petfood/domain/Reviews.java
@@ -25,10 +25,15 @@ public class Reviews {
     private List<Review> reviews = new ArrayList<>();
 
     public double calculateRatingAverage() {
-        return reviews.stream()
+        double average = reviews.stream()
                 .mapToInt(review -> review.getRating())
                 .average()
                 .orElse(0);
+        return roundToOneDecimal(average);
+    }
+
+    private double roundToOneDecimal(double average) {
+        return Math.round(average * 10) / 10.0;
     }
 
     public int countReviews() {
