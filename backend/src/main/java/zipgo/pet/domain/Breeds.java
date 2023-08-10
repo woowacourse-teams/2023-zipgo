4a7f05e9a8b5b53bfa5d82b72c9585ece7ac276d
iamjooon2 <iamjooon2@gmail.com>
2023-08-10 22:20:53 +0900

    refactor: id 제거

--- backend/src/main/java/zipgo/pet/domain/Breeds.java
+++ backend/src/main/java/zipgo/pet/domain/Breeds.java
@@ -3,8 +3,6 @@ package zipgo.pet.domain;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,22 +11,15 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import static jakarta.persistence.EnumType.STRING;
-import static jakarta.persistence.GenerationType.IDENTITY;
-import static lombok.EqualsAndHashCode.Include;
 
 
 @Getter
 @Builder
 @Embeddable
+@EqualsAndHashCode
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Breeds {
-
-    @Id
-    @Include
-    @GeneratedValue(strategy = IDENTITY)
-    private Long id;
 
     @Column(nullable = false)
     private String name;
