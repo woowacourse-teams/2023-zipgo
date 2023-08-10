d766eb817ddf8f81cade75ac5460e31fbc4e7d23
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2023-08-10 22:55:53 +0900

    feat: 필터링 메타데이터 조회 기능

--- backend/src/main/java/zipgo/petfood/domain/repository/PrimaryIngredientRepository.java
+++ backend/src/main/java/zipgo/petfood/domain/repository/PrimaryIngredientRepository.java
@@ -1,8 +1,13 @@
 package zipgo.petfood.domain.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import zipgo.petfood.domain.PrimaryIngredient;
 
 public interface PrimaryIngredientRepository extends JpaRepository<PrimaryIngredient, Long> {
+
+    @Query("select distinct(pi.name) from PrimaryIngredient pi")
+    List<String> findDistinctPrimaryIngredients();
 
 }
