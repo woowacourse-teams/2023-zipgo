850ca93482f48cb8bd3fabb48f110ab2840286fe
n0eyes <doll4586633@naver.com>
2023-08-15 01:04:49 +0900

    feat: SummaryChart 컴포넌트 구현

--- frontend/src/mocks/handlers/review.ts
+++ frontend/src/mocks/handlers/review.ts
@@ -11,6 +11,13 @@ const reviewHandlers = [
 
     return res(ctx.status(200), ctx.json(metaData));
   }),
+
+  rest.get(`${BASE_URL}/reviews/summary?petFoodId`, (req, res, ctx) => {
+    const reviewSummary = reviewFixture.getReviewSummary();
+
+    return res(ctx.delay(2000), ctx.status(200), ctx.json(reviewSummary));
+  }),
+
   rest.get(`${BASE_URL}/reviews/:reviewId`, (req, res, ctx) => {
     const { reviews } = reviewFixture.getReviews();
     const review = reviews.find(review => review.id === Number(req.params.reviewId)) as Review;
@@ -46,12 +53,6 @@ const reviewHandlers = [
   rest.put(`${BASE_URL}/reviews/:reviewId`, async (req, res, ctx) => res(ctx.status(204))),
 
   rest.delete(`${BASE_URL}/reviews/:reviewId`, (req, res, ctx) => res(ctx.status(204))),
-
-  rest.get(`${BASE_URL}/reviews/summary?petFoodId`, (req, res, ctx) => {
-    const reviewSummary = reviewFixture.getReviewSummary();
-
-    return res(ctx.delay(2000), ctx.status(200), ctx.json(reviewSummary));
-  }),
 ];
 
 export default reviewHandlers;
