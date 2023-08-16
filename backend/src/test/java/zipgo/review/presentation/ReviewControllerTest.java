cc47f204258640308d5535d48238328f313f11e7
Rosie <dev.kayoung@gmail.com>
2023-08-16 20:16:14 +0900

    test: 인수테스트 작성
    - 도움이 돼요 추가
    - 작성자가 추가
    - 추가 재시도

--- backend/src/test/java/zipgo/review/presentation/ReviewControllerTest.java
+++ backend/src/test/java/zipgo/review/presentation/ReviewControllerTest.java
@@ -38,6 +38,7 @@ import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.lessThan;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.FORBIDDEN;
@@ -471,6 +472,94 @@ public class ReviewControllerTest extends AcceptanceTest {
                             fieldWithPath("breeds[].name").description("품종 이름").type(JsonFieldType.STRING)
                     )
             );
+        }
+
+    }
+
+    @Nested
+    @DisplayName("리뷰 도움이 돼요 추가 API")
+    class AddHelpfulReaction {
+
+        private Schema 성공_응답_형식 = schema("PostHelpfulReactionResponse");
+        private ResourceSnippetDetails 문서_정보 = resourceDetails().summary("도움이 돼요 추가하기")
+                .description("리뷰에 도움이 돼요를 추가합니다.");
+
+        @Test
+        void 도움이돼요_추가_성공() {
+            //given
+            Member 다른_회원 = memberRepository.save(Member.builder().email("도움이돼요_추가할_회원").name("회원명").build());
+            String 다른_회원의_JWT = jwtProvider.create(다른_회원.getId().toString());
+
+            var 요청_준비 = given().spec(spec)
+                    .contentType(JSON)
+                    .filter(API_문서("리뷰 도움이 돼요 추가 - 성공"));
+
+            //when
+            var 응답 = 요청_준비.when()
+                    .header(AUTHORIZATION, "Bearer " + 다른_회원의_JWT)
+                    .pathParam("reviewId", 리뷰.getId())
+                    .post("/reviews/{reviewId}/helpful-reactions");
+
+            //then
+            응답.then()
+                    .assertThat().statusCode(OK.value());
+        }
+
+        @Test
+        void 작성자가_도움이돼요를_추가하면_예외가_발생() {
+            //given
+            Long 리뷰_작성자_id = 리뷰.getPet().getOwner().getId();
+            String 리뷰_작성자_JWT = jwtProvider.create(리뷰_작성자_id.toString());
+
+            //when
+            var 요청_준비 = given().spec(spec)
+                    .contentType(JSON)
+                    .filter(실패_API_문서("리뷰 도움이 돼요 추가 - 실패 (작성자 본인시도)"));
+
+            //when
+            var 응답 = 요청_준비.when()
+                    .header(AUTHORIZATION, "Bearer " + 리뷰_작성자_JWT)
+                    .pathParam("reviewId", 리뷰.getId())
+                    .post("/reviews/{reviewId}/helpful-reactions");
+
+            //then
+            응답.then()
+                    .assertThat().statusCode(BAD_REQUEST.value());
+        }
+
+        @Test
+        void 이미_눌렀던_리뷰일_경우() {
+            //given
+            Member 다른_회원 = memberRepository.save(Member.builder().email("도움이돼요_추가할_회원").name("회원명").build());
+            String 다른_회원의_JWT = jwtProvider.create(다른_회원.getId().toString());
+
+            spec.contentType(JSON)
+                    .pathParam("reviewId", 리뷰.getId())
+                    .post("/reviews/{reviewId}/helpful-reactions");
+
+            var 요청_준비 = given().spec(spec)
+                    .contentType(JSON)
+                    .filter(실패_API_문서("리뷰 도움이 돼요 추가 - 실패 (이미 누른 리뷰)"));
+
+            //when
+            var 응답 = 요청_준비.when()
+                    .header(AUTHORIZATION, "Bearer " + 다른_회원의_JWT)
+                    .pathParam("reviewId", 리뷰.getId())
+                    .post("/reviews/{reviewId}/helpful-reactions");
+
+            //then
+            응답.then()
+                    .assertThat().statusCode(BAD_REQUEST.value());
+        }
+
+        private RestDocumentationFilter API_문서(String name) {
+            return document(name, 문서_정보.responseSchema(성공_응답_형식),
+                    requestHeaders(headerWithName("Authorization").description("인증을 위한 JWT")),
+                    pathParameters(parameterWithName("reviewId").description("리뷰 id")));
+        }
+
+        private RestDocumentationFilter 실패_API_문서(String name) {
+            return document(name, 문서_정보.responseSchema(에러_응답_형식));
         }
 
     }
