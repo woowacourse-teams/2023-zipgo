ddcb21a9a4f937e81efdbe2a780c14a9f219b80f
iamjooon2 <iamjooon2@gmail.com>
2023-08-16 13:48:02 +0900

    hotfix: AWS SDK 프로필 주입 방법 변경

--- backend/src/main/java/zipgo/common/config/S3Config.java
+++ backend/src/main/java/zipgo/common/config/S3Config.java
@@ -2,7 +2,7 @@ package zipgo.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.InstanceProfileCredentialsProvider;
 import software.amazon.awssdk.services.s3.S3Client;
 
 import static software.amazon.awssdk.regions.Region.AP_NORTHEAST_2;
@@ -13,7 +13,7 @@ public class S3Config {
     @Bean
     public S3Client s3Client() {
         return S3Client.builder()
-                .credentialsProvider(ProfileCredentialsProvider.create())
+                .credentialsProvider(InstanceProfileCredentialsProvider.builder().build())
                 .region(AP_NORTHEAST_2)
                 .build();
     }
