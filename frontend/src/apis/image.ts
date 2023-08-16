8a273e076bf0987071ae81ccb6d568b28680330f
chex <chex1004@gmail.com>
2023-08-16 14:45:58 +0900

    fix/#245: 이미지 api 리퀘스트 값 수정

--- frontend/src/apis/image.ts
+++ frontend/src/apis/image.ts
@@ -2,8 +2,8 @@ import { PostImageFileReq, PostImageFileRes } from '@/types/image/remote';
 
 import { client } from '.';
 
-export const postImageFile = async (postImageFileProps: PostImageFileReq) => {
-  const { data } = await client.post<PostImageFileRes>('/images', postImageFileProps);
+export const postImageFile = async ({ imageFile }: PostImageFileReq) => {
+  const { data } = await client.post<PostImageFileRes>('/images', imageFile);
 
   return data;
 };
