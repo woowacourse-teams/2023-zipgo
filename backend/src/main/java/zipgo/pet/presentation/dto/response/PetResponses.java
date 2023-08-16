c5dc1c00e02a51f53d057404e4cfdf460ce398a6
iamjooon2 <iamjooon2@gmail.com>
2023-08-16 21:41:37 +0900

    test: ControllerTest 구현

--- backend/src/main/java/zipgo/pet/presentation/dto/response/PetResponses.java
+++ backend/src/main/java/zipgo/pet/presentation/dto/response/PetResponses.java
@@ -3,9 +3,9 @@ package zipgo.pet.presentation.dto.response;
 import java.util.List;
 import zipgo.pet.domain.Pet;
 
-public record PetResponses (
-        List<PetResponse> petResponses
-){
+public record PetResponses(
+        List<PetResponse> pets
+) {
 
     public static PetResponses from(List<Pet> pets) {
         return new PetResponses(
