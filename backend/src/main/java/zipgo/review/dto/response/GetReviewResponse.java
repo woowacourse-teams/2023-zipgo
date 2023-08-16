8a45f036a3c5208c73c03da6ddc16349133dcc71
Rosie <dev.kayoung@gmail.com>
2023-08-16 14:58:37 +0900

    feat: dto 정적 팩토리 메서드

--- backend/src/main/java/zipgo/review/dto/response/GetReviewResponse.java
+++ backend/src/main/java/zipgo/review/dto/response/GetReviewResponse.java
@@ -1,10 +1,14 @@
 package zipgo.review.dto.response;
 
 import java.util.List;
+import lombok.Builder;
 import zipgo.review.domain.Review;
+import zipgo.review.domain.repository.dto.FindReviewsQueryResponse;
+import zipgo.review.domain.repository.dto.ReviewWithHelpfulReaction;
 
 import static java.time.format.DateTimeFormatter.ofPattern;
 
+@Builder
 public record GetReviewResponse(
         Long id,
         String reviewerName,
@@ -19,6 +23,7 @@ public record GetReviewResponse(
 
 ) {
 
+    @Builder
     public record PetProfileResponse(
             Long id,
             String name,
@@ -29,14 +34,17 @@ public record GetReviewResponse(
 
     }
 
+    @Builder
     public record BreedResponse(Long id, String name, PetSizeResponse size) {
 
     }
 
+    @Builder
     public record PetSizeResponse(Long id, String name) {
 
     }
 
+    @Builder
     public record HelpfulReactionResponse(Long count, boolean reacted) {
 
     }
@@ -54,6 +62,28 @@ public record GetReviewResponse(
                 null,
                 null
         );
+    }
+
+    public static GetReviewResponse of(FindReviewsQueryResponse review, List<String> adverseReactions,
+                                       ReviewWithHelpfulReaction helpfulReaction) {
+
+        PetSizeResponse petSize = PetSizeResponse.builder().id(review.petSizeId()).name(review.petSizeName()).build();
+        BreedResponse breed = BreedResponse.builder().id(review.breedId()).name(review.breedName()).size(petSize)
+                .build();
+        PetProfileResponse petProfile = PetProfileResponse.builder().name(review.petName()).breed(breed).build();
+        HelpfulReactionResponse helpful = HelpfulReactionResponse.builder()
+                .count(helpfulReaction.helpfulReactionCount()).reacted(helpfulReaction.reacted()).build();
+
+        return GetReviewResponse.builder()
+                .id(review.id())
+                .adverseReactions(adverseReactions)
+                .comment(review.comment())
+                .date(review.date().format(ofPattern("yyyy-MM-dd")))
+                .rating(review.rating())
+                .stoolCondition(review.stoolCondition().getDescription())
+                .petProfile(petProfile)
+                .helpfulReaction(helpful)
+                .build();
     }
 
     private static List<String> getAdverseReactions(Review review) {
