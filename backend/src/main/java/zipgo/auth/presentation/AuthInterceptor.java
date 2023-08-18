fba06cfb4cc6e01d4d1f217908034dd17e48f3f1
iamjooon2 <iamjooon2@gmail.com>
2023-08-18 11:08:00 +0900

    hotfix: Interceptor 내 preflight 검증 로직 추가 (#331)

--- backend/src/main/java/zipgo/auth/presentation/AuthInterceptor.java
+++ backend/src/main/java/zipgo/auth/presentation/AuthInterceptor.java
@@ -2,7 +2,9 @@ package zipgo.auth.presentation;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import zipgo.auth.support.BearerTokenExtractor;
@@ -20,9 +22,32 @@ public class AuthInterceptor implements HandlerInterceptor {
             HttpServletResponse response,
             Object handler
     ) {
+        if (isPreflightRequest(request)) {
+            return true;
+        }
         String accessToken = BearerTokenExtractor.extract(request);
         jwtProvider.validateParseJws(accessToken);
         return true;
+    }
+
+    private boolean isPreflightRequest(HttpServletRequest request) {
+        return isOptions(request) && hasHeaders(request) && hasMethod(request) && hasOrigin(request);
+    }
+
+    private boolean isOptions(HttpServletRequest request) {
+        return request.getMethod().equalsIgnoreCase(HttpMethod.OPTIONS.toString());
+    }
+
+    private boolean hasHeaders(HttpServletRequest request) {
+        return Objects.nonNull(request.getHeader("Access-Control-Request-Headers"));
+    }
+
+    private boolean hasMethod(HttpServletRequest request) {
+        return Objects.nonNull(request.getHeader("Access-Control-Request-Method"));
+    }
+
+    private boolean hasOrigin(HttpServletRequest request) {
+        return Objects.nonNull(request.getHeader("Origin"));
     }
 
 }
