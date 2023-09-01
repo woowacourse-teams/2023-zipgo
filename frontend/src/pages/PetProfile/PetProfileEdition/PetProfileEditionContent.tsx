76dccb4bda1d86e3a41764f342df83dddde939a1
n0eyes <doll4586633@naver.com>
2023-09-01 11:14:16 +0900

    refactor: 불필요한 import 제거

--- frontend/src/pages/PetProfile/PetProfileEdition/PetProfileEditionContent.tsx
+++ frontend/src/pages/PetProfile/PetProfileEdition/PetProfileEditionContent.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useEffect, useState } from 'react';
+import { ChangeEvent, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { styled } from 'styled-components';
 
