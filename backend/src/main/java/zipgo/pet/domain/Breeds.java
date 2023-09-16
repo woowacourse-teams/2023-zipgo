0a76990d1541cc6338d157595a4670085a418618
iamjooon2 <iamjooon2@gmail.com>
2023-09-17 01:13:14 +0900

    refactor: 매직넘버 추출

--- backend/src/main/java/zipgo/pet/domain/Breeds.java
+++ backend/src/main/java/zipgo/pet/domain/Breeds.java
@@ -11,8 +11,8 @@ import static java.util.stream.Collectors.toList;
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class Breeds {
 
+    public static final long MIXED_BREED_ID = 0L;
     private static final int FIRST_PLACE = 0;
-    private static final long MIXED_BREED_ID = 0L;
     private static final String MIXED_BREED_NAME = "믹스견";
     private static final Breed MIXED_BREED = Breed.builder().id(MIXED_BREED_ID).name(MIXED_BREED_NAME).build();
 
