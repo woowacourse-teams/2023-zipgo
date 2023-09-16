234b22c7160453cfe92317ab0f0adbc755223736
iamjooon2 <iamjooon2@gmail.com>
2023-09-17 01:42:28 +0900

    refactor: 반려동물 이름 순 정렬 추가

--- backend/src/main/java/zipgo/pet/domain/Breeds.java
+++ backend/src/main/java/zipgo/pet/domain/Breeds.java
@@ -1,8 +1,8 @@
 package zipgo.pet.domain;
 
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
-
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 
@@ -19,11 +19,16 @@ public class Breeds {
     private final List<Breed> values;
 
     public static Breeds from(List<Breed> breeds) {
-        List<Breed> uniqueBreeds = breeds.stream()
-                .filter(breed -> !breed.getName().equals(MIXED_BREED_NAME))
-                .collect(toList());
+        List<Breed> uniqueBreeds = removeMixBreed(breeds);
+        Collections.sort(uniqueBreeds, Comparator.comparing(Breed::getName));
         uniqueBreeds.add(FIRST_PLACE, MIXED_BREED);
         return new Breeds(uniqueBreeds);
+    }
+
+    private static List<Breed> removeMixBreed(List<Breed> breeds) {
+        return breeds.stream()
+                .filter(breed -> !breed.getName().equals(MIXED_BREED_NAME))
+                .collect(toList());
     }
 
     public List<Breed> getOrderedBreeds() {
