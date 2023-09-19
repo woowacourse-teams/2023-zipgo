8a58ccbe79636b9890f8740d8b981f9232538439
iamjooon2 <iamjooon2@gmail.com>
2023-09-19 20:14:43 +0900

    hotfix: 디렉토리 접두사 제거 (#422)

--- backend/src/main/java/zipgo/image/ImageDirectoryUrl.java
+++ backend/src/main/java/zipgo/image/ImageDirectoryUrl.java
@@ -2,10 +2,9 @@ package zipgo.image;
 
 public class ImageDirectoryUrl {
 
-    public final static String PREFIX_DIRECTORY = "prod/";
     public final static String SUFFIX_DIRECTORY = "-image/";
-    public final static String PET_DIRECTORY = PREFIX_DIRECTORY + "/pet" + SUFFIX_DIRECTORY;
-    public final static String BRAND_DIRECTORY = PREFIX_DIRECTORY + "/brand" + SUFFIX_DIRECTORY;
-    public final static String PET_FOOD_DIRECTORY = PREFIX_DIRECTORY + "/pet-food" + SUFFIX_DIRECTORY;
+    public final static String PET_DIRECTORY = "/pet" + SUFFIX_DIRECTORY;
+    public final static String BRAND_DIRECTORY = "/brand" + SUFFIX_DIRECTORY;
+    public final static String PET_FOOD_DIRECTORY = "/pet-food" + SUFFIX_DIRECTORY;
 
 }
