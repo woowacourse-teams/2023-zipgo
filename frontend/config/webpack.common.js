dcf5dab55109dbb5637d90351f0c3123563613c6
n0eyes <doll4586633@naver.com>
2023-09-21 04:08:10 +0900

    chore: responsive-loader 적용
    - jpg png webp > png webp (사이즈 300px)로 변경

--- frontend/config/webpack.common.js
+++ frontend/config/webpack.common.js
@@ -45,11 +45,37 @@ module.exports = {
         ],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
       },
       {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        test: /\.(jpe?g|png|webp)$/,
+        use: [
+          {
+            loader: 'responsive-loader',
+            options: {
+              adapter: require('responsive-loader/sharp'),
+              sizes: [300],
+              format: 'webp',
+              placeholder: true,
+              placeholderSize: 20,
+            },
+          },
+          {
+            loader: 'responsive-loader',
+            options: {
+              adapter: require('responsive-loader/sharp'),
+              sizes: [300],
+              format: 'png',
+              placeholder: true,
+              placeholderSize: 20,
+            },
+          },
+        ],
+        type: 'javascript/auto',
+      },
+      {
+        test: /\.(gif|svg)$/i,
         type: 'asset/resource',
       },
     ],
