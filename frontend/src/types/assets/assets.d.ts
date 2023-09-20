c5687f01b2e2ad860eda0bb81cf53e95e5f3ee0e
n0eyes <doll4586633@naver.com>
2023-09-21 04:10:28 +0900

    chore: asset의 postfix를 통해 responsive-loader output 타입을 적용할 수 있도록 타입 선언

--- frontend/src/types/assets/assets.d.ts
+++ frontend/src/types/assets/assets.d.ts
@@ -1,7 +1,25 @@
-declare module '*.png';
 declare module '*.svg';
 declare module '*.woff';
 declare module '*.woff2';
 declare module '*.eot';
 declare module '*.ttf';
 declare module '*.otf';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.png';
+declare module '*.webp';
+
+declare module '*responsive' {
+  const content: ResponsiveImageOutput;
+  export default content;
+}
+
+interface ResponsiveImageOutput {
+  src: string;
+  srcSet: string;
+  placeholder: string | undefined;
+  images: { path: string; width: number; height: number }[];
+  width: number;
+  height: number;
+  toString: () => string;
+}
