1c569aaa801778bf28a8724221aceb1ce9d6c3bc
Seyeon Jeong <79056677+n0eyes@users.noreply.github.com>
2023-09-22 11:15:58 +0900

    feature: responsive-loader, sharp 제거 후 webp 수동 전환 (#440)
    * chore: docker base image 변경
    
    * chore: responsive-loader, sharp 제거
    
    * refactor: assets.d.ts reponsive declare type 제거
    
    * refactor: 변경된(webp) 배너 이미지로 변경
    
    * chore: eslint rule 추가
    
    * chore: asset contenthash 추가
    
    * fix: 변경된 배너이미지 preload

--- frontend/src/types/assets/assets.d.ts
+++ frontend/src/types/assets/assets.d.ts
@@ -8,18 +8,3 @@ declare module '*.jpg';
 declare module '*.jpeg';
 declare module '*.png';
 declare module '*.webp';
-
-declare module '*responsive' {
-  const content: ResponsiveImageOutput;
-  export default content;
-}
-
-interface ResponsiveImageOutput {
-  src: string;
-  srcSet: string;
-  placeholder: string | undefined;
-  images: { path: string; width: number; height: number }[];
-  width: number;
-  height: number;
-  toString: () => string;
-}
