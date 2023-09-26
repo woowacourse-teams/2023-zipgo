68358dcdd4d3fbbabf210ea7b2e6f31a1edc0aaa
iamjooon2 <iamjooon2@gmail.com>
2023-09-26 15:17:44 +0900

    test: 로그아웃 쿠키 테스트 추가

--- backend/src/test/java/zipgo/auth/support/RefreshTokenCookieProviderTest.java
+++ backend/src/test/java/zipgo/auth/support/RefreshTokenCookieProviderTest.java
@@ -2,25 +2,23 @@ package zipgo.auth.support;
 
 import java.time.Duration;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
-import jakarta.servlet.http.Cookie;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.ResponseCookie;
 import zipgo.common.config.JwtCredentials;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
 @SuppressWarnings("NonAsciiCharacters")
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class RefreshTokenCookieProviderTest {
 
-    private static final JwtCredentials testJwtCredentials =  new JwtCredentials(
+    private static final JwtCredentials testJwtCredentials = new JwtCredentials(
             "집사의고민시크릿키",
             1000000000L,
-            60 * 60 * 24 * 7 * 2// 일주일
+            60 * 60 * 24 * 7 * 2 // 일주일
     );
 
     @Test
@@ -38,6 +36,21 @@ class RefreshTokenCookieProviderTest {
                 () -> assertThat(cookie.isHttpOnly()).isTrue(),
                 () -> assertThat(cookie.getMaxAge()).isEqualTo(Duration.ofMillis(60 * 60 * 24 * 7 * 2)),
                 () -> assertThat(cookie.getValue()).isEqualTo("refreshToken")
+        );
+    }
+
+    @Test
+    void 로그아웃_쿠키를_생성한다() {
+        // given
+        RefreshTokenCookieProvider refreshTokenCookieProvider = new RefreshTokenCookieProvider(testJwtCredentials);
+
+        // when
+        ResponseCookie logoutCookie = refreshTokenCookieProvider.createLogoutCookie();
+
+        // then
+        assertAll(
+                () -> assertThat(logoutCookie.getValue()).isEqualTo("refreshToken"),
+                () -> assertThat(logoutCookie.getMaxAge()).isZero()
         );
     }
 
