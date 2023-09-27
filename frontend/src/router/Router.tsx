494595a169a74668ab8303051d20511e833b6d1c
n0eyes <doll4586633@naver.com>
2023-09-27 14:34:57 +0900

    refactor: exception routes에 NotFound 페이지 적용

--- frontend/src/router/Router.tsx
+++ frontend/src/router/Router.tsx
@@ -93,7 +93,7 @@ export const router = createBrowserRouter([
       },
       {
         path: PATH.EXCEPTION,
-        element: <ErrorPage />,
+        element: <ErrorPage.NotFound />,
       },
     ],
   },
