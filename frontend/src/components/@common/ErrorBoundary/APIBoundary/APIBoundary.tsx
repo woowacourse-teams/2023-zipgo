521d6d796fb058fe12fcf5352283d776fe19a51e
n0eyes <doll4586633@naver.com>
2023-10-04 04:56:17 +0900

    refactor: APIBoundary에서 401 핸들링을 하도록 변경

--- frontend/src/components/@common/ErrorBoundary/APIBoundary/APIBoundary.tsx
+++ frontend/src/components/@common/ErrorBoundary/APIBoundary/APIBoundary.tsx
@@ -1,19 +1,34 @@
 import { ComponentProps, PropsWithChildren } from 'react';
+import { Navigate } from 'react-router-dom';
 
+import { PATH } from '@/router/routes';
+import { resolveRenderProps } from '@/utils/compound';
 import { APIError, ZipgoError } from '@/utils/errors';
 
 import { ErrorBoundary } from '../ErrorBoundary';
 
-type APIBoundaryProps = ComponentProps<typeof ErrorBoundary>;
+type APIBoundaryProps = ComponentProps<typeof ErrorBoundary<APIError>>;
 
 const APIBoundary = (props: PropsWithChildren<APIBoundaryProps>) => {
-  const handleAPIError = (error: unknown) => {
+  const { fallback, ...restProps } = props;
+
+  const handleAPIError: APIBoundaryProps['onError'] = error => {
     const convertedError = ZipgoError.convertToError(error);
 
     if (!(convertedError instanceof APIError)) throw convertedError;
   };
 
-  return <ErrorBoundary onError={handleAPIError} {...props} />;
+  const handleAPIFallback: APIBoundaryProps['fallback'] = ({ error, reset }) =>
+    /** @todo 추후 에러 코드 상의 후 변경 */
+    error.status === 401 ? (
+      <Navigate to={PATH.LOGIN} />
+    ) : (
+      resolveRenderProps(fallback, { error, reset })
+    );
+
+  return (
+    <ErrorBoundary<APIError> onError={handleAPIError} fallback={handleAPIFallback} {...restProps} />
+  );
 };
 
 export type { APIBoundaryProps };
