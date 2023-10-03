29f14208bfd3684e5d8621e189c3ef1179f8cef2
chex <chex1004@gmail.com>
2023-10-03 15:51:46 +0900

    feat/#457: 사료 선택 도움말 배너  컴포넌트 추가

---
+++ frontend/src/components/FoodSelectionGuideBanner/FoodSelectionGuideBanner.tsx
@@ -0,0 +1,145 @@
+import styled from 'styled-components';
+
+import CloseSquareIcon from '@/assets/svg/close_square_icon_light.svg';
+import WidthPetIcon from '@/assets/svg/with_pet_icon.svg';
+import useBoolean from '@/hooks/@common/useBoolean';
+
+const FoodSelectionGuideBanner = () => {
+  const [isOpen, , closeBanner] = useBoolean(true);
+
+  if (!isOpen) return null;
+
+  return (
+    <BannerWrapper>
+      <a
+        href="https://translucent-mallet-426.notion.site/dae305b85c8146399d7de6a0e74b773d?pvs=4"
+        target="blank"
+      >
+        <img src={WidthPetIcon} alt="" />
+        <span>
+          To. 초보집사
+          <br />
+          우리 아이 먹거리 고르는 꿀팁 보러오세요!
+        </span>
+      </a>
+      <button type="button" aria-label="닫기" onClick={closeBanner}>
+        <img src={CloseSquareIcon} alt="" />
+      </button>
+    </BannerWrapper>
+  );
+};
+
+export default FoodSelectionGuideBanner;
+
+const BannerWrapper = styled.div`
+  user-select: none;
+
+  position: fixed;
+  z-index: 100;
+  bottom: 2rem;
+  left: 50%;
+  transform-origin: center center;
+  transform: translateX(-50%);
+
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 90vw;
+  height: 8rem;
+
+  background-color: ${({ theme }) => theme.color.black};
+  border: none;
+  border-radius: 8px;
+
+  transition: all 0.3s ease-out;
+
+  & a {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    width: 100%;
+    height: 100%;
+
+    font-size: 2.2rem;
+    font-weight: 500;
+    line-height: 2.4rem;
+    color: ${({ theme }) => theme.color.white};
+  }
+
+  & img {
+    transform: translateY(12%);
+
+    margin-right: 1.2rem;
+  }
+
+  & span {
+    width: 100%;
+    padding-right: 1.2rem;
+  }
+
+  & button {
+    cursor: pointer;
+
+    position: absolute;
+    top: 0.4rem;
+    right: -0.4rem;
+
+    color: ${({ theme }) => theme.color.white};
+
+    background-color: transparent;
+    border: none;
+    outline: none;
+  }
+
+  &:hover::before {
+    content: '';
+
+    position: absolute;
+    left: 50%;
+    transform-origin: center center;
+    transform: translateX(-50%);
+
+    display: inline-block;
+
+    width: 80vw;
+    height: 8rem;
+
+    opacity: 0;
+    background-color: ${({ theme }) => theme.color.white};
+
+    animation: shiny 2.5s ease-in-out infinite;
+
+    @keyframes shiny {
+      0% {
+        transform: scale(0) rotate(45deg);
+
+        opacity: 0;
+      }
+
+      80% {
+        transform: scale(0) rotate(45deg);
+
+        opacity: 0.5;
+      }
+
+      81% {
+        transform: scale(4) rotate(45deg);
+
+        opacity: 1;
+      }
+
+      100% {
+        transform: scale(50) rotate(45deg);
+
+        opacity: 0;
+      }
+    }
+  }
+
+  &:active {
+    transform: translateX(-50%) scale(0.95);
+  }
+`;
