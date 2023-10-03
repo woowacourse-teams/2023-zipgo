758585ec9f75ea92810009a7fa4eca057f23d0fc
n0eyes <doll4586633@naver.com>
2023-10-04 05:21:43 +0900

    refactor: APIBoundary 불필요한 에러 재변환 제거

--- frontend/src/components/@common/ErrorBoundary/APIBoundary/APIBoundary.tsx
+++ frontend/src/components/@common/ErrorBoundary/APIBoundary/APIBoundary.tsx
@@ -3,7 +3,7 @@ import { Navigate } from 'react-router-dom';
 
 import { PATH } from '@/router/routes';
 import { resolveRenderProps } from '@/utils/compound';
-import { APIError, ZipgoError } from '@/utils/errors';
+import { APIError, UnexpectedError } from '@/utils/errors';
 
 import { ErrorBoundary } from '../ErrorBoundary';
 
@@ -12,10 +12,8 @@ type APIBoundaryProps = ComponentProps<typeof ErrorBoundary<APIError>>;
 const APIBoundary = (props: PropsWithChildren<APIBoundaryProps>) => {
   const { fallback, ...restProps } = props;
 
-  const handleAPIError: APIBoundaryProps['onError'] = error => {
-    const convertedError = ZipgoError.convertToError(error);
-
-    if (!(convertedError instanceof APIError)) throw convertedError;
+  const handleAPIError: APIBoundaryProps['onError'] = (error: APIError | UnexpectedError) => {
+    if (!(error instanceof APIError)) throw error;
   };
 
   const handleAPIFallback: APIBoundaryProps['fallback'] = ({ error, reset }) =>
