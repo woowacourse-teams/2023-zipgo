8157e53b8b735a33050392604b133ca9284f7e94
n0eyes <doll4586633@naver.com>
2023-10-18 16:24:43 +0900

    refactor: 불필요한 axios instance 설정 제거

--- frontend/src/apis/index.ts
+++ frontend/src/apis/index.ts
@@ -1,32 +1,19 @@
 import axios, { AxiosError, AxiosRequestConfig } from 'axios';
 
-import { Tokens } from '@/types/auth/client';
+import { APIError } from '@/utils/errors';
 import { zipgoLocalStorage } from '@/utils/localStorage';
 
 import { refreshZipgoAuth } from './auth';
 
 export const { BASE_URL } = process.env;
 
-const tokens = zipgoLocalStorage.getTokens();
-
 const defaultConfig: AxiosRequestConfig = {
   baseURL: BASE_URL,
-  withCredentials: true,
 };
-
-export const createConfigWithAuth = (tokens: Tokens | null) =>
-  tokens
-    ? {
-        ...defaultConfig,
-        headers: {
-          Authorization: `Bearer ${tokens.accessToken}`,
-        },
-      }
-    : defaultConfig;
 
 export const clientBasic = axios.create(defaultConfig);
 
-export const client = axios.create(createConfigWithAuth(tokens));
+export const client = axios.create(defaultConfig);
 
 client.interceptors.request.use(config => {
   const tokens = zipgoLocalStorage.getTokens();
@@ -44,23 +31,19 @@ client.interceptors.response.use(
   async (error: AxiosError) => {
     const config = error.config;
 
-    if (error?.response?.status === 401) {
-      if (!config?.headers.Authorization) {
-        alert('로그인 후 이용해 주세요');
-      } else {
-        try {
-          const { accessToken } = await refreshZipgoAuth();
+    if (APIError.isAuthError(error) && config?.headers.Authorization) {
+      try {
+        const { accessToken } = await refreshZipgoAuth();
 
-          zipgoLocalStorage.setTokens({ accessToken });
+        zipgoLocalStorage.setTokens({ accessToken });
 
-          config.headers.Authorization = `Bearer ${accessToken}`;
+        config.headers.Authorization = `Bearer ${accessToken}`;
 
-          return client(config);
-        } catch (error) {
-          alert('세션이 만료되었습니다.');
+        return client(config);
+      } catch (error) {
+        alert('세션이 만료되었습니다');
 
-          zipgoLocalStorage.clearAuth();
-        }
+        zipgoLocalStorage.clearAuth();
       }
     }
 
