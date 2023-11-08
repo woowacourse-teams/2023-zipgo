4920c4c9a89d65cee032ce8d1586bc5601ca71eb
iamjooon2 <iamjooon2@gmail.com>
2023-11-08 15:16:04 +0900

    feat: 견종 목록 읽기시 캐싱 적용

--- backend/src/main/java/zipgo/pet/domain/repository/BreedRepository.java
+++ backend/src/main/java/zipgo/pet/domain/repository/BreedRepository.java
@@ -2,12 +2,19 @@ package zipgo.pet.domain.repository;
 
 import java.util.List;
 import java.util.Optional;
+
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import zipgo.pet.domain.Breed;
 import zipgo.pet.domain.PetSize;
 import zipgo.pet.exception.BreedsNotFoundException;
 
 public interface BreedRepository extends JpaRepository<Breed, Long> {
+
+    @Cacheable(cacheNames = "breeds", key = "'allBreeds'")
+    default List<Breed> findAllBreeds() {
+        return findAll();
+    }
 
     Optional<Breed> findByPetSizeAndName(PetSize petSize, String name);
 
